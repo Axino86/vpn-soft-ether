da3a8cbc8bdcc295dbd3cb155b7ea9bfc6294fc9
Axino86 <50746121+Axino86@users.noreply.github.com>
2025-05-26 16:29:47 +0500

    Update updateHosts.sh
    - Использовать расширенные глобальные и РФ/СНГ-списки блокировки из Скрипт 1  
    - Генерировать `~/hosts.supp` и копировать в `/etc/hosts.supp` как в Скрипт 3  
    - Добавлена строгая обработка ошибок, проверка прав root и автоматическая очистка временных файлов

--- updateHosts.sh
+++ updateHosts.sh
@@ -1,76 +1,96 @@
 #!/usr/bin/env bash
-# Last revision: 2021/02/12
-# Script originally belongs to SteveRiley, thanks for the work.
-# I just did some modifications, bug fixing etc.
+# updateHosts.sh — генератор ~/hosts.supp для ad-blocking через DNSMasq
+# Алгоритм из Скрипта 1, генерация итогового файла и пути как в Скрипте 3
+# Автор: Александр / 2025
 
-# Let's create our temporary files
+set -euo pipefail
+IFS=$'\n\t'
+
+##############################
+# Проверка прав суперпользователя
+##############################
+if [[ $EUID -ne 0 ]]; then
+  echo "[ERROR] Запуск скрипта возможен только от root." >&2
+  exit 1
+fi
+
+##############################
+# Подготовка временных файлов
+##############################
 temphosts1=$(mktemp)
 temphosts2=$(mktemp)
+suppfile=~/hosts.supp
 
-# Obtain various hosts files and merge into one
-# You can add/remove more as you wish.
-echo "Downloading ad-blocking hosts files..."
-curl -ksS http://winhelp2002.mvps.org/hosts.txt > $temphosts1
-curl -ksS http://someonewhocares.org/hosts/zero/hosts >> $temphosts1
-curl -ksS "http://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&mimetype=plaintext&useip=0.0.0.0" >> $temphosts1
-curl -ksS https://raw.githubusercontent.com/nomadturk/vpn-adblock/master/hosts-sources/Turkish/hosts >> $temphosts1
-curl -ksS https://adaway.org/hosts.txt >> $temphosts1
-curl -ksS http://www.malwaredomainlist.com/hostslist/hosts.txt >> $temphosts1
-# curl -ksS http://hosts-file.net/ad_servers.txt >> $temphosts1
-curl -ksS https://raw.githubusercontent.com/nomadturk/vpn-adblock/master/hosts-sources/nomad/hosts  >> $temphosts1
-# https://github.com/Ultimate-Hosts-Blacklist/Ultimate.Hosts.Blacklist
-# curl -ksS https://hosts.ubuntu101.co.za/hosts >> $temphosts1
+cleanup() {
+  rm -f "$temphosts1" "$temphosts2" "${suppfile}.tmp"
+}
+trap cleanup EXIT
 
-# Do some work on the file:
-# 1. Remove MS-DOS carriage returns
-# 2. Delete all lines that don't begin with 127.0.0.1 or 0.0.0.0
-# 3. Delete any lines containing the word localhost because we'll obtain that from the original hosts file
-# 4. Replace 127.0.0.1 with 0.0.0.0 because then we don't have to wait for the resolver to fail
-# 5. Scrunch extraneous spaces separating address from name into a single tab
-# 6. Delete any comments on lines
-# 7. Clean up leftover trailing blanks
-# 8. Finally, delete all lines that don't begin with 0.0.0.0 to make sure that all remnants are removed
-# Pass all this through sort with the unique flag to remove duplicates and save the result
-echo 
-echo "Parsing" 
-echo "Cleaning"
-echo "De-duplicating"
-echo "Sorting..."
-echo 
-sed -e 's/\r//'                         \
-    -e '/^127.0.0.1\|0.0.0.0/!d'        \
-    -e '/localhost/d'                   \
-    -e 's/127.0.0.1/0.0.0.0/'           \
-    -e 's/ \+/\t/'                      \
-    -e 's/#.*$//'                       \
-    -e 's/[ \t]*$//'                    \
-    -e '/^0.0.0.0/!d'                   \
-     < $temphosts1 | sort -u > $temphosts2
+##############################
+# Источники блокировки рекламы и трекеров
+##############################
+sources=(
+  # Основные глобальные списки
+  "https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts"
+  "https://blocklistproject.github.io/Lists/ads.txt"
+  "https://raw.githubusercontent.com/notracking/hosts-blocklists/master/hostnames.txt"
+  "https://raw.githubusercontent.com/jerryn70/GoodbyeAds/master/Hosts/GoodbyeAds.txt"
+  "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/hosts/pro.txt"
+  "https://raw.githubusercontent.com/lightswitch05/hosts/master/ads-and-tracking-extended.txt"
 
-# Remove 0.0.0.0 accidentally referring to itself
-sed -i 's/^0.0.0.0\\t0.0.0.0/0.0.0.0/' $temphosts2
+  # Дополнительные источники для РФ/СНГ
+  "https://someonewhocares.org/hosts/zero/hosts"
+  "https://raw.githubusercontent.com/nomadturk/vpn-adblock/master/hosts-sources/nomad/hosts"
+  "https://raw.githubusercontent.com/Perflyst/PiHoleBlocklist/master/android-tracking.txt"
+  "https://raw.githubusercontent.com/Perflyst/PiHoleBlocklist/master/SmartTV.txt"
+  "https://raw.githubusercontent.com/ookangzheng/dbl-oisd-nl/master/dbl.txt"
+)
 
-# Combine system hosts with adblocks
-echo "Creating supplementary hosts file is complete..."
-echo -e "#<local-data>" > ~/hosts.supp
-echo -e "\n# Ad blocking hosts generated "$(date) >> ~/hosts.supp
-cat $temphosts2 > ~/hosts.supp.tmp
-totallines="$(grep --regexp="$" --count ~/hosts.supp.tmp)"
-echo -e "# Total number of blocked hosts are:" $totallines >> ~/hosts.supp
-rm -rf ~/hosts.supp.tmp
-cat $temphosts2 >> ~/hosts.supp
-echo -e "#</local-data>" >> ~/hosts.supp
-# Clean up temp files and remind user to copy new file
-echo "Time to clean up..."
-rm $temphosts1 $temphosts2
-echo "Done."
-echo
-echo "Copying ad-blocking hosts file with this command:"
-echo "We are going to use a supplementary file named hosts.supp"
-echo "This is required to not to mess with the existing hosts file"
-echo "cp ~/hosts.supp /etc"
-cat ~/hosts.supp > /etc/hosts.supp
-echo
-echo "You can always delete or update this file or just remove any references"
-echo "from DNSMasq or whatever you use it with"
-echo
+echo "[INFO] Скачивание списков блокировки..."
+for url in "${sources[@]}"; do
+  echo "→ $url"
+  if ! curl --fail --silent --show-error "$url" >> "$temphosts1"; then
+    echo "[WARN] Не удалось получить $url" >&2
+  fi
+done
+
+##############################
+# Очистка, нормализация, сортировка
+##############################
+echo "[INFO] Очистка и фильтрация..."
+sed -e 's/\r//' \
+    -e '/^127\.0\.0\.1\|^0\.0\.0\.0/!d' \
+    -e '/localhost/d' \
+    -e 's/^127\.0\.0\.1/0.0.0.0/' \
+    -e 's/[[:space:]]\+/\t/' \
+    -e 's/#.*$//' \
+    -e 's/[[:space:]]*$//' \
+    -e '/^0\.0\.0\.0/!d' \
+    "$temphosts1" | sort -u > "$temphosts2"
+
+# Удаление самоссылок
+sed -i 's/^0\.0\.0\.0\t0\.0\.0\.0/0.0.0.0/' "$temphosts2"
+
+##############################
+# Генерация итогового файла
+##############################
+echo "[INFO] Создание итогового файла: $suppfile"
+echo -e "#<local-data>" > "$suppfile"
+echo -e "\n# Ad blocking hosts generated $(date)" >> "$suppfile"
+
+# Временный файл для подсчёта строк
+cat "$temphosts2" > "${suppfile}.tmp"
+total=$(grep --regexp="\$" --count "${suppfile}.tmp")
+
+echo -e "# Total number of blocked hosts are: $total" >> "$suppfile"
+cat "$temphosts2" >> "$suppfile"
+echo -e "#</local-data>" >> "$suppfile"
+
+##############################
+# Копирование в /etc и завершение
+##############################
+echo "[INFO] Копирование $suppfile → /etc/hosts.supp"
+cp "$suppfile" /etc/hosts.supp
+
+echo "[OK] Файл создан: /etc/hosts.supp"
+echo "Перезапустите DNSMasq: systemctl restart dnsmasq"
